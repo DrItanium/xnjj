15e34229e7aaf9fa5060a0801757651c7b7e02da
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-06-01 16:17:50 -0700

    Update config.mk to reflect c++

--- config.mk
+++ config.mk
@@ -18,20 +18,18 @@ TERMINAL = xterm
 
 # Flags
 include $(ROOT)/mk/gcc.mk
-CFLAGS += -Os $(DEBUGCFLAGS)
+CXXFLAGS += -Os $(DEBUGCXXFLAGS)
 LDFLAGS += -g
 
 # Compiler, Linker. Linker should usually *not* be ld.
-CC = cc -c
-LD = cc
+CXX = c++ -c
+LD = c++
 # Archiver
 AR = ar crs
 
-PYTHON = python
-
 X11PACKAGES = x11 xinerama xrender xrandr
 INCX11 = $$(pkg-config --cflags $(X11PACKAGES))
-LIBIXP = $(LIBDIR)/libixp.a
+LIBJYQ = $(LIBDIR)/libjyq.a
 
 # Enable RTLD. Only necessary for Xft support.
 CFLAGS += -DHAVE_RTLD
@@ -40,30 +38,3 @@ LDFLAGS += -ldl # Comment this out on BSD systems.
 SOLDFLAGS += $(LDFLAGS)
 SHARED = -shared -Wl,-soname=$(SONAME)
 STATIC = -static
-
-# Your make shell. By default, the first found of /bin/dash, /bin/ksh,
-# /bin/sh. Except with bsdmake, which assumes /bin/sh is sane. bash and zsh
-# are painfully slow, and should be avoided.
-#BINSH = /bin/ash
-
-## Operating System Configurations
-
-# KenCC
-# Note: wmii *must* always compile under KenCC. It's vital for
-# argument checking in formatted IO, and similar diagnostics.
-#CFLAGS = -wF
-#STATIC = # Implied
-#CC=pcc -c
-#LD=pcc
-
-# Darwin
-#STATIC = # Darwin doesn't like static linking
-#SHARED = -dynamiclib
-#SOEXT = dylib
-
-# Solaris
-#SHELL = /bin/bash
-#CFLAGS = -fast $(INCS)
-#LDFLAGS = $(LIBS) -R$(PREFIX)/lib -lsocket -lnsl
-#CFLAGS += -xtarget=ultra
-
